e8e906abce7a5af6aa31e28a8bb704f0c04ce92a
Josua <josuasitanggang51@gmail.com>
2023-03-11 11:01:57 +0700

    add: page 404

---
+++ src/Components/PageNotFound.tsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <p className="text-2xl font-bold">Page Not Found</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
