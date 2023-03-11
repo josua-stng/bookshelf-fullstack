3c73f3cf2e0d7828d7c1ad101d2a3394c8896626
Josua <josuasitanggang51@gmail.com>
2023-03-11 11:02:58 +0700

    fix routing & add routing protect and page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import { Route, Routes } from "react-router-dom"
-import LoginPage from "./Authentication/Login"
-import Register from "./Authentication/Register"
-import Addbooks from "./book-management/AddBooks"
-import EditBooks from "./book-management/EditBooks"
-import Bookshelf from "./Components/Bookshelf"
+import { Route, Routes } from "react-router-dom";
+import AuthGuard from "./Authentication/AuthGuard";
+import LoginPage from "./Authentication/Login";
+import Register from "./Authentication/Register";
+import Addbooks from "./book-management/AddBooks";
+import EditBooks from "./book-management/EditBooks";
+import Bookshelf from "./Components/Bookshelf";
+import PageNotFound from "./Components/PageNotFound";
+import ProtectRoutes from "./Components/ProtectRoutes";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<LoginPage/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/bookshelf" element={<Bookshelf/>}/>
-      <Route path="/books/add" element={<Addbooks/>}/>
-      <Route path="/books/:id" element={<EditBooks/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/bookshelf"
+          element={
+            <ProtectRoutes>
+              <Bookshelf />
+            </ProtectRoutes>
+          }
+        />
+
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/warning" element={<AuthGuard />} />
+        <Route path="/books/add" element={<Addbooks />} />
+        <Route path="/books/:id" element={<EditBooks />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
